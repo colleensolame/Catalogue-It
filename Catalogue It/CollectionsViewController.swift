a1ed5ff4c0576e1183c6bdf3ede93b3641e414ad
Colleen Ng <colleenkyng@gmail.com>
2017-09-20 01:03:38 +0800

    Basic adding new collections and basic deleting collections

---
+++ Catalogue It/CollectionsViewController.swift
@@ -0,0 +1,90 @@
+//
+//  CollectionsViewController.swift
+//  Catalogue It
+//
+//  Created by Colleen Ng on 9/20/17.
+//  Copyright © 2017 ZND Code. All rights reserved.
+//
+
+import UIKit
+
+class CollectionsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    var collections: [String] = []
+    
+    @IBOutlet weak var collectionsTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionsTableView.dataSource = self
+        collectionsTableView.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return collections.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = collections[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    @IBAction func btnLogOut(_ sender: Any) {
+    }
+    
+    @IBAction func btnAdd(_ sender: Any) {
+        let alertController = UIAlertController(title: "Create New Collection", message: nil, preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "Create", style: .default) { (_) in
+            if alertController.textFields?[0].text != "" {
+                self.collections.append((alertController.textFields?[0].text)!)
+                self.collectionsTableView.reloadData()
+            } else {
+                self.collections.append("(empty)")
+                self.collectionsTableView.reloadData()
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
+        
+        
+        alertController.addTextField(configurationHandler: {(textField : UITextField!) -> Void in
+            textField.placeholder = "New Collection"
+        })
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            /*let collection = collections[indexPath.row]
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(sound)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            do {
+                try sounds = context.fetch(Sound.fetchRequest())
+                collectionsTableView.reloadData()
+            } catch {}*/
+            collections.remove(at: indexPath.row)
+            collectionsTableView.reloadData()
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
--- Catalogue It/LoginViewController.swift
+++ Catalogue It/LoginViewController.swift
@@ -7,19 +7,63 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func btnLogin(_ sender: Any) {
+        
+        performSegue(withIdentifier: "collectionsSegue", sender: nil)
+        
+        /*if (txtEmail.text?.isEmpty == false && txtPassword.text?.isEmpty == false) {
+            let databaseRef = Auth.auth().
+            
+            databaseRef.child("Users").observeSingleEventOfType(FIRDataEventType.Value, withBlock: { (snapshot) in
+                
+                if snapshot.hasChild(self.usernameTextField.text!){
+                    
+                    print("true rooms exist")
+                    
+                }else{
+                    
+                    print("false room doesn't exist")
+                }
+                
+                
+            })*/
+        /*Auth.auth().signIn(withEmail: txtEmail.text!, password: txtPassword.text!) { (user, error) in
+            print("We tried to sign in")
+            if error != nil {
+                print("We have an error: \(String(describing: error))")
+                let alertController = UIAlertController(title: "Email not found", message: "Please register an account first", preferredStyle: .alert)
+                
+                let okAction = UIAlertAction(title: "Ok", style: .default) { (_) in }
+                
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+                
+                
+            } else {
+                print("We have sign in successfully")
+            }
+        }*/
+    }
+    
+    @IBAction func btnSignUp(_ sender: Any) {
+    }
 }
 
