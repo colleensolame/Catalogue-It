a7266822033c61894b9f451b2fec13e4ccd4923a
Colleen Ng <colleenkyng@gmail.com>
2017-09-20 21:40:55 +0800

    Completed Sign Up and Login Screen, including validations, all connected to Firebase

--- Catalogue It/LoginViewController.swift
+++ Catalogue It/LoginViewController.swift
@@ -20,58 +20,41 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     @IBAction func btnLogin(_ sender: Any) {
         
-        
-        
         if (txtEmail.text?.isEmpty == true) || (txtPassword.text?.isEmpty == true) {
+            //Validation : Check if all fields are entered
+            
             let alert = UIAlertController(title: "Login Failed", message: "Please enter email and password", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
             
-        /*} else if {*/
-            
         } else {
-            performSegue(withIdentifier: "collectionsSegue", sender: nil)
+            
+            // Sign in, check if email is registered in database
+            Auth.auth().signIn(withEmail: txtEmail.text!, password: txtPassword.text!) { (user, error) in
+                
+                if error != nil {
+                    // Email is not found in database or email and password do not match
+                    
+                    print("We have an error: \(String(describing: error))")
+                    let alertController = UIAlertController(title: "Login Failed", message: "Invalid Email/Password", preferredStyle: .alert)
+                    
+                    let okAction = UIAlertAction(title: "Ok", style: .default) { (_) in }
+                    
+                    alertController.addAction(okAction)
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                    
+                } else {
+                    
+                    print("We have sign in successfully")
+                    self.performSegue(withIdentifier: "collectionsSegue", sender: nil)
+                }
+            }
+            
         }
         
-        /*
-            let databaseRef = Auth.auth().
-            
-            databaseRef.child("Users").observeSingleEventOfType(FIRDataEventType.Value, withBlock: { (snapshot) in
-                
-                if snapshot.hasChild(self.usernameTextField.text!){
-                    
-                    print("true rooms exist")
-                    
-                }else{
-                    
-                    print("false room doesn't exist")
-                }
-                
-                
-            })*/
-        /*Auth.auth().signIn(withEmail: txtEmail.text!, password: txtPassword.text!) { (user, error) in
-            print("We tried to sign in")
-            if error != nil {
-                print("We have an error: \(String(describing: error))")
-                let alertController = UIAlertController(title: "Email not found", message: "Please register an account first", preferredStyle: .alert)
-                
-                let okAction = UIAlertAction(title: "Ok", style: .default) { (_) in }
-                
-                alertController.addAction(okAction)
-                self.present(alertController, animated: true, completion: nil)
-                
-                
-            } else {
-                print("We have sign in successfully")
-            }
-        }*/
     }
     
     @IBAction func btnSignUp(_ sender: Any) {
--- Catalogue It/SignUpViewController.swift
+++ Catalogue It/SignUpViewController.swift
@@ -12,7 +12,9 @@ import FirebaseDatabase
 
 class SignUpViewController: UIViewController {
     
-    @IBOutlet weak var txtUsername: UITextField!
+    let set = NSCharacterSet.letters.inverted   // Range for text input allowed. Only characters
+    
+    @IBOutlet weak var txtFullName: UITextField!
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet weak var txtConfirmPassword: UITextField!
@@ -22,24 +24,35 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        lblSuccess.isHidden = true
+        lblSuccess.isHidden = true   // Hide success button until account has been successfully created
         
     }
     
+    
     @IBAction func btnBack(_ sender: Any) {
-        performSegue(withIdentifier: "loginSegue", sender: nil)
+        performSegue(withIdentifier: "loginSegue", sender: nil)    // Segue back to login screen
     }
     
     @IBAction func btnSignUp(_ sender: Any) {
         
-        if (txtUsername.text?.isEmpty == true) || (txtEmail.text?.isEmpty == true) || (txtPassword.text?.isEmpty == true) || (txtConfirmPassword.text?.isEmpty == true) {
+        if (txtFullName.text?.isEmpty == true) || (txtEmail.text?.isEmpty == true) || (txtPassword.text?.isEmpty == true) || (txtConfirmPassword.text?.isEmpty == true) {
+            // Validation: Check if all fields are entered
             
             lblSuccess.isHidden = true
             let alert = UIAlertController(title: "Sign Up Failed", message: "Please enter all the fields", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
             
+        } else if txtFullName.text!.rangeOfCharacter(from: set) != nil {
+            // Validation: Check if Full Name contains numbers or special characters
+            
+            lblSuccess.isHidden = true
+            let alert = UIAlertController(title: "Sign Up Failed", message: "Full Name cannot contain numbers or special characters", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            
         } else if txtPassword.text != txtConfirmPassword.text {
+            // Validation: Check if password and confirm password matches
             
             lblSuccess.isHidden = true
             let alert = UIAlertController(title: "Sign Up Failed", message: "Your passwords do not match", preferredStyle: .alert)
@@ -47,6 +60,7 @@ class SignUpViewController: UIViewController {
             self.present(alert, animated: true, completion: nil)
             
         } else if (txtPassword.text?.characters.count)! < 6 {
+            // Validation: Check if password is more than 6 characters
             
             lblSuccess.isHidden = true
             let alert = UIAlertController(title: "Sign Up Failed", message: "Password has to be more than 6 characters", preferredStyle: .alert)
@@ -55,26 +69,33 @@ class SignUpViewController: UIViewController {
             
         } else {
             
-            buttonSignUp.isEnabled = false
+            buttonSignUp.isEnabled = false    // Disables sign up button until sign up process is complete
             
+            // Create new user in Firebase
             Auth.auth().createUser(withEmail: txtEmail.text!, password: txtPassword.text!, completion: { (user, error) in
+                
+                // Error in email formatting or email already in database
                 if error != nil {
                     
+                    print(error as Any)
                     self.lblSuccess.isHidden = true
-                    let alert = UIAlertController(title: "Sign Up Failed", message: "The email address is already in use", preferredStyle: .alert)
+                    let alert = UIAlertController(title: "Invalid Email", message: "The email given is wrong or is already in use", preferredStyle: .alert)
                     alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
                     self.present(alert, animated: true, completion: nil)
                     self.buttonSignUp.isEnabled = true
                     
                 } else {
                     
+                    // Adding user's Full Name to the database
                     let changeRequest = user!.createProfileChangeRequest()
-                    changeRequest.displayName = self.txtUsername.text
+                    changeRequest.displayName = self.txtFullName.text
                     changeRequest.commitChanges(completion: { (error) in
+                        
                         if error != nil {
                             print("We have another error: \(String(describing: error))")
                         } else {
                             
+                            // Adding user information to database
                             Database.database().reference().child("users").child(user!.uid).child("Full Name").child((user!.displayName)!).child("email").setValue(user!.email!)
                             self.buttonSignUp.isEnabled = true
                             self.lblSuccess.isHidden = false
